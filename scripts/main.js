5faa161344a4f380ccb72d7a0c13092736efd3ae
Khang Dinh Hoang <khangdh95@gmail.com>
2019-05-07 14:56:13 +0700

    .

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+initMain = function() {
+    window.initAutoScaleCenterGLTF();
+}
+
+initAutoScaleCenterGLTF = function() {
+    AFRAME.registerComponent('autoscale', {
+        schema: {type: 'number', default: 1},
+        init: function () {
+          this.scale();
+          this.el.addEventListener('object3dset', () => this.scale());
+
+          this.el.addEventListener('click', () => this.onClickModel());
+
+        },
+        scale: function () {
+          const el = this.el;
+          const span = this.data;
+          const mesh = el.getObject3D('mesh');
+      
+          if (!mesh) return;
+      
+          // Compute bounds.
+          const bbox = new THREE.Box3().setFromObject(mesh);
+      
+          // Normalize scale.
+          const scale = span / bbox.getSize().length();
+          mesh.scale.set(scale, scale, scale);
+      
+          // Recenter.
+        //   const offset = bbox.getCenter().multiplyScalar(scale);
+        //   mesh.position.sub(offset);
+        },
+        onClickModel: function() {// (evt) is always null
+            console.log("clicked: ");
+            console.log(this.el);
+        }
+      });
+}
+
+
+
+
+window.initMain();
