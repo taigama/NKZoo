698f0c5abc6703f92256c1c2867d4155d27b32d2
nam <tatanoi96@gmail.com>
2019-06-07 18:38:56 +0700

    temp

--- test/scripts/entity-event.js
+++ test/scripts/entity-event.js
@@ -17,13 +17,51 @@ AFRAME.registerComponent('kscale', {
         // Normalize scale.
         const scale = span / bbox.getSize().length();
         mesh.scale.set(scale, scale, scale);
-        console.log(JSON.stringify(mesh.scale));
-        console.log(JSON.stringify(mesh.position));
     },
 });
 
-AFRAME.registerComponent('clone', {
-    init: () => {
-
+AFRAME.registerComponent('register-events', {
+    init: function () {
+        this.el.addEventListener('markerFound', this.onMarkerFound);
+        this.el.addEventListener('markerLost', this.onMarkerLost);
+    },
+    onMarkerFound: function () {
+        currentElement = this;
+        console.log('found=======================================================');
+        console.log(`position=${JSON.stringify(currentElement.object3D.position)}`);
+        console.log(`rotation=${JSON.stringify(currentElement.object3D.rotation)}`);
+        console.log(`scale=${JSON.stringify(currentElement.object3D.scale)}`);
+    },
+    onMarkerLost: function () {
+        console.log('lost');
     }
 });
+
+let currentElement = null;
+
+function appendHtml(el, str) {
+    let div = document.createElement('div');
+    div.innerHTML = str;
+    while (div.children.length > 0) {
+        el.appendChild(div.children[0]);
+    }
+}
+
+function save() {
+    if (currentElement === null || currentElement === undefined) {
+        console.log("No element was found on camera");
+        return;
+    }
+    let scene = document.getElementById('scene');
+    let position = currentElement.object3D.position;
+    let rotation = currentElement.object3D.rotation;
+    let scale = currentElement.object3D.scale;
+    let attribute = "";
+    attribute = attribute + ` position="${position.x} ${position.y} ${position.z}" `;
+    attribute = attribute + ` rotation="${rotation.x} ${rotation.y} ${rotation.z}" `;
+    attribute = attribute + ` scale="${scale.x} ${scale.y} ${scale.z}" `;
+
+    appendHtml(scene, `<a-entity gltf-model="#butterfly" ${attribute} kscale="4" animation-mixer shadow="cast: true; receive: true;"></a-entity>`);
+}
+
+window.save = save;
